df7577e8c371673a2bf46ad7227356a41fcdfb92
Roman Luštrik <romunov@gmail.com>
2017-06-19 17:07:45 +0200

    improved microsatTabExtract

--- microsatTabExtract.R
+++ microsatTabExtract.R
@@ -1,3 +1,4 @@
+#' Extract
 microsatTabExtract <- function(filename, sampleName) {
   #### extract data according to sample name and print these data ordered by counts
   ## the script keeps the two first columns : id and count (updated by the script)
@@ -15,33 +16,40 @@ microsatTabExtract <- function(filename, sampleName) {
   #   print ("2- the abbreviated name used in the file for the samples (ex : CLF or UAS")
   #   stop()
   # }
+  # #### read file and get data
+  # data <- read.table(filename, header = TRUE)
+  # nom <- colnames(data)
+  # ext <- sapply(nom, function(x) strsplit(x,'_')[[1]][1])
+  # 
+  # extSamplesNames <- sapply(ext, function(x) substr(x, 8, nchar(x)))
+  # sampleColNumbers <- which(extSamplesNames == sampleName)
+  # id <- data$id
+  # sequence <- data$sequence
+  # seq_length <- data$seq_length
+  # dfExt <- data[, sampleColNumbers]
+  # dfExtColNames <- colnames(dfExt)
+  # 
+  # ext2 <- as.numeric(sapply(dfExtColNames, function(x) strsplit(x, '_')[[1]][2]))
+  # sampleColNumbers2 <- which(ext2 > 0)
+  # dfExt2 <- dfExt[, sampleColNumbers2]
+  # count <- apply(dfExt2, MARGIN = 1, FUN = sum)
+  # 
+  # #### create dataframe and print to output file
+  # df <- data.frame(id, count, dfExt2, seq_length, sequence)
+  # df <- df[order(df$count, decreasing = TRUE), ]
+  # fileoutname <- paste("MICROSAT.PCR", "_", sampleName, "_", strsplit(basename(filename), "_")[[1]][4], sep = "")
+  xy <- read.table(filename, header = TRUE)
   
-  # filename <- args[1]
-  # sampleName <- args[2]
+  samcols <- colnames(xy)[grepl(sampleName, names(xy))]
+  out <- xy[, c("id", "count", samcols, "seq_length", "sequence")]
+  fileoutname <- sprintf("MICROSAT.PCR_%s_%s.uniq.tab",
+                         sampleName, 
+                         # extract locus name
+                         gsub("^MICROSAT\\.PCR_.*_([[:alnum:]]+)\\.uniq\\.tab$", "\\1", basename(filename)))
   
-  #### read file and get data
-  data <- read.table(filename, header = TRUE)
-  nom <- colnames(data)
-  ext <- sapply(nom, function(x) strsplit(x,'_')[[1]][1])
   
-  extSamplesNames <- sapply(ext, function(x) substr(x, 8, nchar(x)))
-  sampleColNumbers <- which(extSamplesNames == sampleName)
-  id <- data$id
-  sequence <- data$sequence
-  seq_length <- data$seq_length
-  dfExt <- data[, sampleColNumbers]
-  dfExtColNames <- colnames(dfExt)
+  out <- out[order(out$count, decreasing = TRUE), ] 
+  out$count <- rowSums(out[, samcols])
   
-  ext2 <- as.numeric(sapply(dfExtColNames, function(x) strsplit(x, '_')[[1]][2]))
-  sampleColNumbers2 <- which(ext2 > 0)
-  dfExt2 <- dfExt[, sampleColNumbers2]
-  count <- apply(dfExt2, MARGIN = 1, FUN = sum)
-  
-  #### create dataframe and print to output file
-  # df <- as.data.frame(cbind(id, count, dfExt2, seq_length, sequence))
-  df <- data.frame(id, count, dfExt2, seq_length, sequence)
-  df <- df[order(df$count, decreasing = TRUE), ]
-  fileoutname <- paste("MICROSAT.PCR", "_", sampleName, "_", strsplit(filename, "_")[[1]][4], sep = "")
-  
-  write.table(df, fileoutname, quote = FALSE, row.names = FALSE, sep = "\t")
+  write.table(out, file = fileoutname, quote = FALSE, row.names = FALSE, sep = "\t")
 }
