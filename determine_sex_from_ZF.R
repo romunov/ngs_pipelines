10f8c241f77de96e7f8bb2489660aafde11812a4
Roman Luštrik <romunov@gmail.com>
2017-08-04 16:43:44 +0200

    try to determine sex based on chisq.test

--- determine_sex_from_ZF.R
+++ determine_sex_from_ZF.R
@@ -2,6 +2,8 @@
 # Input is the folder where _ZF.uniq.tab files (as produced by the last command in the pipeline - obitab).
 
 library(data.table)
+library(ggplot2)
+library(plotly)
 
 #### INPUT ####
 # point this line to the folder where .uniqe.tab files of ZF are located
@@ -57,12 +59,52 @@ sexy[sequence %in% seqUAY, sex := "Y"]
 fwrite(sexy, file = file.out)
 
 threshold <- 0.5
-x <- sexy[Sample_Name %in% "M00CL", -"sequence"]
-x <- x[-2]
-x <- x[order(Sample_Name, run, sex)]
-x[, xrle := seq(1:.N), by = .(run)]
-x <- dcast(x, run + Sample_Name + seq_length + position + library ~ xrle, value.var = c("count", "sex"))
-x[, ratio := count_1/count_2]
-x[, sex := ifelse(ratio > threshold, "F", "M")]
-x[, sex := ifelse(is.na(sex), "C", sex)]
-x
+# x <- sexy[Sample_Name %in% "M00CL", -"sequence"]
+# x <- x[order(Sample_Name, run, sex)]
+# x[, xrle := seq(1:.N), by = .(run)]
+# x <- dcast(x, run + Sample_Name + seq_length + position + library ~ xrle, value.var = c("count", "sex"))
+# x[, ratio := count_1/count_2]
+# x[, sex := ifelse(ratio > threshold, "F", "M")]
+# x[, sex := ifelse(is.na(sex), "C", sex)]
+# x
+
+### learn of ratio significance
+lrn <- fread("./DAB/barkode_spol.txt")
+# find samples where sex is known
+known.sex <- sexy[sexy$Sample_Name %in% lrn$Sample_Name, ]
+# merge with genetic data
+known.sex <- known.sex[lrn, on = "Sample_Name"]
+# known.sex <- known.sex[order(Sample_Name, run, sex)]
+# reshape data to better fit our "wide" model
+known.sex <- dcast(known.sex, run + Sample_Name + seq_length + position + library + truesex ~ sex, 
+                   value.var = c("count"))
+# # sort so that X and Y are consistently placed
+known.sex <- known.sex[order(Sample_Name, run), ]
+# replace missing values with zeros - useful later
+known.sex[, X := ifelse(is.na(X), 0, X)]
+known.sex[, Y := ifelse(is.na(Y), 0, Y)]
+# calculate ratio and round to two digits
+known.sex[, ratio := round(X/Y, 2)]
+
+known.sex[, pval := chisq.test(c(X, Y), p = c(0.5, 0.5))$p.value, by = 1:nrow(known.sex)]
+
+
+
+
+
+# write (intermediate) result to file
+fwrite(known.sex, file = "./DAB/genetic_sex_compare_to_true.txt")
+
+ggplotly(ggplot(known.sex, aes(x = truesex, y = ratio, group = Sample_Name)) +
+  theme_bw() +
+  geom_jitter()
+)
+
+chisq.test(c(370, 353), p = c(0.5, 0.5))
+chisq.test(c(700, 0), p = c(0.999, 0.001))
+chisq.test(c(1200, 0), p = c(0.999, 0.001))
+chisq.test(c(199, 155), p = c(0.5, 0.5))$p.value
+
+
+
+
