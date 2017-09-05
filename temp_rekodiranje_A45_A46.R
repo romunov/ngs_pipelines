7d53bbb55e3d163e2bad7f37059e0cdc1b79c2aa
romunov <romunov@gmail.com>
2017-09-05 22:12:18 +0200

    merganje sprememb v surove podatke

--- temp_rekodiranje_A45_A46.R
+++ temp_rekodiranje_A45_A46.R
@@ -20,10 +20,20 @@ ap27$Sample_Name <- mapvalues(x = ap27$Sample_Name, from = rc27$before,
 # The following `from` values were not present in `x`: M1Y3A
 # M1Y3A se pretvori iz prave F3 v G8 (AK.05AJ), ki pa je v naši poziciji blank
 
-ap18[Position == "034", -c("TagCombo", "Sequence", "called", "flag", "length")]
+ap18[Position == "055", -c("TagCombo", "Sequence", "called", "flag", "length")]
+ap27[Position == "055", -c("TagCombo", "Sequence", "called", "flag", "length")]
 
 ap <- rbindlist(list(ap18, ap27))
+
 
 ap[Position == "034", ]
 fwrite(ap, file = "./DAB/data/dab_hiseq2_A45_A46_new_genotypes.txt",
        sep = "\t")
+
+# dodaj te podatke v surove
+
+fp <- xy2[!(Run_Name == "DAB23" & Plate %in% c(1, 2, 7, 8)), ]
+fp[, .N, by = .(Run_Name, Plate)][order(Run_Name), ]
+otu <- rbindlist(list(fp, ap))
+otu[, .N, by = .(Run_Name, Plate)][order(Run_Name), ]
+fwrite(otu, file = "./DAB/data/dab_hiseq2_genotypes.txt", sep = "\t")
